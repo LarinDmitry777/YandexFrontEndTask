e8675f03f26e37b16d2458da4d4bcf9ff857d671
LarinDmitry <larin.dmitry.7@ya.ru>
2020-03-26 22:47:45 +0500

    Fix errors

--- src/controllers/errors.ts
+++ src/controllers/errors.ts
@@ -1,22 +1,20 @@
-import {NextFunction, Request, Response} from 'express';
+import {Request, Response} from 'express';
 
 export class PageError extends Error{
 }
 
-export function processError404(err: Error, _req: Request, res: Response, next: NextFunction): void {
+export function processError404(err: Error, _req: Request, res: Response): void {
     if (err instanceof PageError && err.message === '404'){
         console.error(err);
         res.sendStatus(404);
     }
-    next()
 }
 
-export function processError500(err: Error, _req: Request, res: Response, next: NextFunction): void {
+export function processError500(err: Error, _req: Request, res: Response): void {
     if (err instanceof PageError && err.message === '500'){
         console.error(err);
         res.sendStatus(500);
     }
-    next()
 }
 
 

4b3523fc14b043df5e0e6788df2524797d0872a5
LarinDmitry <larin.dmitry.7@ya.ru>
2020-03-28 19:30:38 +0500

    Now css files are going to bundles

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -2,13 +2,13 @@ rm -rf ./dist/public
 rm -rf ./dist/views
 cp -r ./src/public ./dist/public
 
-mkdir ./dist/public
-mkdir ./dist/public/styles
 mkdir ./dist/views
 mkdir ./dist/views/partials
 
-cp ./src/views/*.css ./dist/public/styles/
-cp ./src/views/partials/*.css ./dist/public/styles/
-
 cp -r ./src/views/*.hbs ./dist/views
 cp -r ./src/views/partials/*.hbs ./dist/views/partials
+
+mkdir ./dist/public
+mkdir ./dist/public/bundles
+
+npx create-bundles
